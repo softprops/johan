ad2cab0925aadce94f09ea02a52ffe3b537df8de
softprops <d.tangren@gmail.com>
2015-02-10 23:59:12 -0500

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# johan
+
+> j(s)o(n) in. han(dlebars) out.
+
+Doug Tangren (softprops) 2015
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate "rustc-serialize" as serialize;
+extern crate handlebars;
+
+use std::old_io as io;
+use serialize::json::Json;
+use handlebars::Handlebars;
+
+fn main() {
+   let input: String = if io::stdio::stdin_raw().isatty() {
+     io::stdin().read_line().ok().expect("json expected")
+   } else {
+     "{}".to_string()
+   };
+   let json: Json = Json::from_str(&input).ok().expect("malformed json");
+   let mut handlebars = Handlebars::new();
+   handlebars.register_template_string("t","hello {{foo}}".to_string()).ok().expect("expected template");
+   println!("{}", handlebars.render("t", &json).ok().unwrap());
+}
