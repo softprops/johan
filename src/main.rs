aabbe3f231404e67bcb01455ea9493f60cc545f1
softprops <d.tangren@gmail.com>
2015-02-11 00:00:47 -0500

    annot not needed

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use serialize::json::Json;
 use handlebars::Handlebars;
 
 fn main() {
-   let input: String = if io::stdio::stdin_raw().isatty() {
+   let input = if io::stdio::stdin_raw().isatty() {
      io::stdin().read_line().ok().expect("json expected")
    } else {
      "{}".to_string()
    };
-   let json: Json = Json::from_str(&input).ok().expect("malformed json");
+   let json = Json::from_str(&input).ok().expect("malformed json");
    let mut handlebars = Handlebars::new();
    handlebars.register_template_string("t","hello {{foo}}".to_string()).ok().expect("expected template");
    println!("{}", handlebars.render("t", &json).ok().unwrap());
