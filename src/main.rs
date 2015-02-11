06372028d6bcd3d38dd92482b125b82bd93048e9
softprops <d.tangren@gmail.com>
2015-02-11 00:43:01 -0500

    shorter lines

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,30 @@ use std::os;
 
 fn main() {
    let template = match os::args().as_slice() {
-      [_, ref bars] => io::File::open(&Path::new(&bars)).ok().expect("invalid path").read_to_string().ok().expect("invalid file"),
+      [_, ref bars] =>
+          io::File::open(&Path::new(&bars))
+            .ok()
+            .expect("invalid path")
+            .read_to_string()
+            .ok()
+            .expect("invalid file"),
            _ => "{{.}}".to_string()
    };
    let input = if io::stdio::stdin_raw().isatty() {
      "{}".to_string()
    } else {
-     io::stdin().read_line().ok().expect("json expected")
+     io::stdin()
+       .read_line()
+       .ok()
+       .expect("json expected")
    };
-   let json = Json::from_str(&input).ok().expect("malformed json");
+   let json = Json::from_str(&input)
+     .ok()
+     .expect("malformed json");
    let mut handlebars = Handlebars::new();
-   handlebars.register_template_string("t", template).ok().expect("expected template");
-   println!("{}", handlebars.render("t", &json).ok().unwrap());
+   handlebars.register_template_string("t", template)
+     .ok()
+     .expect("expected template");
+   println!("{}", handlebars.render("t", &json)
+                  .ok().unwrap());
 }
