a31ed964b0fe8e68eb34fe2b99c6e49eb50d66cd
softprops <d.tangren@gmail.com>
2015-07-09 23:23:50 -0400

    improvements

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,71 @@
-#![feature(old_io, plugin)]
-#![plugin(docopt_macros)]
-
-extern crate "rustc-serialize" as rustc_serialize;
-extern crate docopt;
+extern crate atty;
+extern crate rustc_serialize;
+extern crate clap;
 extern crate handlebars;
 
+use clap::App;
 use handlebars::Handlebars;
 use rustc_serialize::json::Json;
 use std::fs::File;
-use std::path::Path;
 use std::io;
-use std::old_io;
-use std::io::Read;
-use std::env;
-
-//docopt!(Args derive Debug, "
-//Usage: johan [options] [-]
-//       johan (--help | --version)
-
-//Options:
-//  -h, --help          show this help content
-//  -f, --file FILE     path to handlebars (.hbs) template
-//  -t, --template STR  a raw handlebars template string
-//  -j, --json STR      json data
-//", flag_file: Option<Path>, flag_template: Option<String>, flag_json: Option<//String>);
+use std::io::{Read, Write};
+use std::process::exit;
 
 fn main() {
 
-  //let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
-  //println!("args {:?}", args);
-  
-  let template: Option<String> = match env::args_os().as_slice() {
-      [_, ref bars] =>
+  fn ok<S: Into<String>>(txt: S) {
+    write!(&mut std::io::stdout(), "{}", txt.into());
+    exit(0)
+  }
+
+  fn err<S: Into<String>>(txt: S) {
+    write!(&mut std::io::stderr(), "{}", txt.into());
+    exit(1)
+  }
+
+  let matches = App::new("johan")
+    .about("applies json data to handlebars templates")
+    .args_from_usage(
+      "-t, --template=[TEMPLATE] 'handlebars template path. defaults to handlebars src {{.}}'
+       [INPUT] 'json data. will read from std input'"
+     )
+    .get_matches();
+  let bars  = matches.value_of("TEMPLATE");
+  let input = matches.value_of("INPUT").map(|i| i.to_owned()).or_else(|| {
+    if atty::is() {
+      let mut raw = String::new();
+      io::stdin()
+        .read_line(&mut raw)
+        .map(|_| raw)
+        .ok()
+    } else {
+      None
+    }
+  });
+
+  let template: Option<String> =
+    match bars {
+      Some(bars) =>
         File::open(bars)
-          .and_then(|mut f| {
-              let mut contents = String::new();
-              f.read_to_string(&mut contents).map(|_| contents)
-          })
-          .ok(),
-       _ =>
-         Some("{{.}}".to_string())
+      .and_then(|mut f| {
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).map(|_| contents)
+      })
+      .ok(),
+    _ =>
+      Some("{{.}}".to_string())
    };
 
-   let opt: Option<String> = if old_io::stdio::stdin_raw().isatty() {
-     Some("{}".to_string())
-   } else {
-     let mut raw = String::new();
-     io::stdin()
-       .read_line(&mut raw)
-       .map(|_| raw)
-       .ok()
-   };
-   let json = opt.and_then(|input| Json::from_str(&input).ok());
+  let json = input.and_then(|input| Json::from_str(&input).ok());
 
-   let mut handlebars = Handlebars::new();
-   let rendered = template
-      .and_then(|bars| handlebars.register_template_string("t", bars).ok())
-      .and_then(|_| json)
-      .and_then(|json| handlebars.render("t", &json).ok());
+  let mut handlebars = Handlebars::new();
+  let rendered = template
+    .and_then(|bars| handlebars.register_template_string("t", bars).ok())
+    .and_then(|_| json)
+    .and_then(|json| handlebars.render("t", &json).ok());
 
     match rendered {
-      Some(r) => println!("{}", r),
-      _ => println!("failed to render")
+      Some(r) => ok(r),
+      _ => err("failed to render")
     }
 }
